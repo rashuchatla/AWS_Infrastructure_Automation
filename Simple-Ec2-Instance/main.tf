19e0ffb1aeacfb1402fe24c5fc58737ed2728cb9
Rashmitha Chatla <chatla.rashmitha@oracle.com>
2023-11-05 01:18:41 +0530

    Initial commit - create basic ec2 instance on AWS

--- Simple-Ec2-Instance/main.tf
+++ Simple-Ec2-Instance/main.tf
@@ -7,7 +7,3 @@ module "ec2_instance"{
    ami = "<>"
    instance_type = "t2.micro"
 }
-
-#A project - whenever a src code commit is issued, build the project using GitHub Actions and use terraform to 
-# spin up Ec2 Instance with required VPC and security groups and deploy the Flask App and access it using instance
-#public ip
